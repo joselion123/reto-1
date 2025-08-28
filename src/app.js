e89fdc2f2c74f7cd7afd030c1c39cc8cbc7b2b32
joselion123 <joselion528@gmail.com>
2025-08-28 13:17:36 -0500

    a

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/proveedor', proveedorRoutes);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
